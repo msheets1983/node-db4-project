7045e33304dc7c6748c80a0c33c1c3bf2e6f5530
Michael Sheets <michaelsheets@MacBook-Pro.local>
2020-08-26 21:54:00 -0500

    initial commit

---
+++ index.js
@@ -0,0 +1,8 @@
+const express = require("express");
+
+const server = express();
+const port = process.env.PORT || 5000;
+
+server.listen(port, () => {
+  console.log(`Running at http://localhost:${port}`);
+});
